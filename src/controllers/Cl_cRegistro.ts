fa138c3fb464bbb9e9a1b1e98e0ccba05f2bf802
Alcy Villanueva <lopezalcy53@gmail.com>
2026-03-14 15:58:33 -0400

    Enunciado

--- src/controllers/Cl_cRegistro.ts
+++ src/controllers/Cl_cRegistro.ts
@@ -1,8 +1,15 @@
 /*
   EL PROBLEMA A RESOLVER:
-  Sea el nombre, sexo y edad de varias personas. Se necesita un programa que lea estos datos
-  y reporte al final la cantidad de hombres, mujeres, total y datos del mayor.
-*/
+Sea el nombre y el sexo de varias personas (F-M). Se necesita un programa que lea estos datos
+y reporte al final la cantidad de hombres y la cantidad de mujeres procesadas.
+Se tienen las siguientes personas: Luisa, Ana, José, Carmen, Rosa, José, María, Luz, 
+Rafael, Liz, Marcos y Leo; la salida requerida presenta el siguiente formato:
+Cantidad de personas: 12
+Cantidad de hombres: 5
+Cantidad de mujeres: 7
+  NUEVO REQUERIMIENTO: 
+Se requiere que el programa indique cuál es el sexo con mayor edad.
+ */
 
 // =========================================
 // EL CONTROLADOR PRINCIPAL: "EL JEFE MAYOR"
@@ -18,7 +25,7 @@ import Cl_mPersona from "../models/Cl_mPersona.js";
 export default class Cl_cRegistro {
   // El Jefe Mayor contrata a su Contador Matemático (para hacer cuentas)
   private mRegistro: Cl_mRegistro = new Cl_mRegistro();
-  
+
   // El Jefe Mayor contrata a su Pantalla Principal (para mostrar resultados)
   private vRegistro: Cl_vRegistro = new Cl_vRegistro();
 
@@ -37,10 +44,10 @@ export default class Cl_cRegistro {
     new Cl_cPersona({
       // 2. Le deja una instrucción (callback): "Cuando termines, mándame la tarjeta con los datos (p)".
       callback: (p: Cl_mPersona | null) => {
-        
+
         // 3. Si la tarjeta NO está vacía (es decir, el usuario le dio a "Aceptar" y no a "Cancelar")...
         if (p !== null) {
-          
+
           // A) Le pasa la tarjeta al Contador Matemático para que sume y haga sus cálculos.
           this.mRegistro.procesarPersona(p);
 
@@ -49,18 +56,18 @@ export default class Cl_cRegistro {
 
           // C) Le pide al Contador Matemático los números finales y le dice a la Pantalla Principal que los muestre en el cuadro azul.
           this.vRegistro.mostrarTotales(
-             // "Oye contador, ¿cuántos hay en total?"
+            // "Oye contador, ¿cuántos hay en total?"
             this.mRegistro.totalPersonas(),
-            
+
             // "Oye contador, ¿cuántas mujeres?"
             this.mRegistro.totalMujeres,
-            
+
             // "Oye contador, ¿cuántos hombres?"
             this.mRegistro.totalHombres,
-            
+
             // "Oye contador, ¿qué edad tiene el mayor?"
             this.mRegistro.edadMayor,
-            
+
             // "Oye contador, ¿de qué sexo es?"
             this.mRegistro.sexoMayor,
           );
